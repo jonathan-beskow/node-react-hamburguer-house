7ff14ffaa796c792eb8ecee0f8b5b1c2a2e5188a
M373746 <j.beskow@sistran.com.br>
2026-07-07 14:49:07 -0300

    adding new component2

---
+++ frontend/src/components/header/Header.tsx
@@ -0,0 +1,14 @@
+import logo from '../../assets/logo.png'
+
+
+export function Header() {
+    return (
+        <div className="bg-[#161410]">
+            <div className="w-full md:w-184.25 p-3 md:p-0 mx-auto flex items-center justify-between">
+                <img src={logo} />
+                <div className="bg-[#F2DAAC] w-32.5 h-8.75 flex items-center justify-center rounded-sm cursor-pointer">Entrar</div>
+            </div>
+        </div>
+
+    );
+}
