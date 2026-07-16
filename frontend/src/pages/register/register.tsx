46ae7fa0feb27a9fa21cf5fd40e28e9dbda8b0ea
M373746 <j.beskow@sistran.com.br>
2026-07-16 09:31:16 -0300

    implementing register functions

--- frontend/src/pages/register/register.tsx
+++ frontend/src/pages/register/register.tsx
@@ -8,32 +8,83 @@ import { Input } from '../../components/input/input';
 
 export default function Register() {
 
-    const [nome, setNome] = useState("");
+    const [name, setName] = useState("");
     const [email, setEmail] = useState("");
-    const [senha, setSenha] = useState("");
+    const [password, setPassword] = useState("");
     const [confirmsenha, setConfirmsenha] = useState("");
     const [cep, setCep] = useState("");
+    const [error, setError] = useState("");
 
-    function handleSubmit(e: React.FormEvent<HTMLFormElement>) {
+    async function handleSubmit(e: React.FormEvent<HTMLFormElement>) {
         e.preventDefault();
-        console.log(email);
-        console.log(senha);
+        try {
+            verifyInfo();
+            const response = await sendRequest('http://localhost:3000/register', 'POST', { name, email, password, cep })
+            switch (response.status) {
+                case 409:
+                    setError("E-mail já cadastrado");
+                    break;
+                case 400:
+                    setError("Todas as informações são obrigatórias");
+                    break;
+                case 201:
+                    setName("");
+                    setEmail("");
+                    setPassword("");
+                    setConfirmsenha("")
+                    setCep("");
+                    break;
+                case 500:
+                    setError("Tente novamente mais tarde");
+                    break;
+                default:
+                    setError("");
+            }
+
+        } catch (error) {
+            console.log;
+            return;
+        }
+    }
+
+    async function sendRequest(rota: string, method: string, body: Object) {
+        return fetch(rota, {
+            method: method,
+            headers: { "Content-type": "application/json" },
+            body: JSON.stringify(body)
+        })
+    }
+
+    function verifyInfo() {
+        if (!name || !email || !password || !cep) {
+            setError("Todas as informações são obrigatórias")
+            return;
+        }
+        if (password !== confirmsenha) {
+            setError("Senhas não conferem")
+            return
+        }
+        return true;
     }
 
 
     return (
         <form className="flex h-screen items-center justify-center bg-[#161410]" onSubmit={handleSubmit}>
-            <div className="flex flex-col items-center justify-center gap-2">
-                <Link to="/"><img src={logo} className='mb-4' /></Link>
-                <Input type="text" title="Digite seu nome..." onChange={(e) => setNome(e.target.value)} />
-                <Input type="email" title="E-mail..." onChange={(e) => setEmail(e.target.value)} />
-                <Input type="password" title="Digite sua senha..." onChange={(e) => setSenha(e.target.value)} />
-                <Input type="password" title="Confirme sua senha..." onChange={(e) => setConfirmsenha(e.target.value)} />
-                <Input type="text" title="Digite seu CEP..." onChange={(e) => setCep(e.target.value)} />
-                <Button title='Criar Conta' />
-                <Link to="/login" className='w-full'>
-                    <Button title='Já tenho uma conta' variant='outline' />
-                </Link>
+            <div className="flex flex-col justify-center gap-2">
+                <Link to="/"><img src={logo} className='mb-4 mx-auto' /></Link>
+                <Input type="text" value={name} title="Digite seu nome..." onChange={(e) => setName(e.target.value)} />
+                <Input type="email" value={email} title="E-mail..." onChange={(e) => setEmail(e.target.value)} />
+                <Input type="password" value={confirmsenha} title="Digite sua senha..." onChange={(e) => setPassword(e.target.value)} />
+                <Input type="password" value={password} title="Confirme sua senha..." onChange={(e) => setConfirmsenha(e.target.value)} />
+                <Input type="text" value={cep} title="Digite seu CEP..." onChange={(e) => setCep(e.target.value)} />
+                <p className='font-bold text-red-500'>{error}</p>
+                <div className='w-full mt-2 gap-2 flex flex-col'>
+                    <Button title='Criar Conta' type='submit' />
+                    <Link to="/login" className='w-full'>
+                        <Button title='Já tenho uma conta' variant='outline' />
+                    </Link>
+                </div>
+
             </div>
 
         </form>
